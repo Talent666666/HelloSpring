859edf2bdd26177ed0d5834a709cddcea5708931
Penguins <talent.zl@qq.com>
2023-02-21 23:00:09 +0800

    Initial commit

---
+++ src/net/biancheng/HelloWorld.java
@@ -0,0 +1,13 @@
+package net.biancheng;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("message : " + message);
+    }
+}
